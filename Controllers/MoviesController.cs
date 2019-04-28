72197b89c5fdddb58af67f8efce510b235af9477
Zeronly <zeronly@hotmail.com>
2019-04-27 21:47:28 -0400

    Modificado --> 27.04.2019

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var movie = new CMovie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
